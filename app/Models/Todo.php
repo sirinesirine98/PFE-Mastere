81f5dbd7f26d952c9048aa6086bbc08107063599
SirineBenAoune <etudiant.benaoune.sirine@uvt.tn>
2023-02-24 14:12:58 +0100

    first commit

---
+++ app/Models/Todo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    use HasFactory;
+    #si notre id n'est pas auto incrément on doit ajouter cette ligne :  public $incrementing = false;
+}
